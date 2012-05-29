6ea2c5da356cc3ddccbd99fbb285e2cebe7bb011
Karthik Ram <karthik.ram@gmail.com>
2012-05-29 11:58:42 -0700

    updated task handling for empty projects.

--- R/tasks.R
+++ R/tasks.R
@@ -8,31 +8,30 @@
 #' @examples \dontrun{
 #' pending_tasks(1235, token)
 #'}
-incomplete_tasks <- function(project_id, token = getOption('TodoistToken'), recurring = FALSE) {
-ptasks_url <- "https://todoist.com/API/getUncompletedItems"
-args <- list(project_id = project_id, token = token)
-tasks <- getForm(ptasks_url, .params = args)
-
-format_tasks <- function(foo, recurring) {
-	result <- data.frame(id=foo$id, name=foo$content, due=foo$date, priority = foo$priority, pid = foo$project_id, ds = foo$date_string)
+incomplete_tasks <- function(project_id, token = getOption("TodoistToken"),
+    recurring = FALSE) {
+    ptasks_url <- "https://todoist.com/API/getUncompletedItems"
+    args <- list(project_id = project_id, token = token)
+    tasks <- getForm(ptasks_url, .params = args)
+    format_tasks <- function(foo, recurring) {
+        result <- data.frame(id = foo$id, name = foo$content, due = foo$date, priority = foo$priority,
+            pid = foo$project_id, ds = foo$date_string)
+    }
+    my_tasks <- fromJSON(tasks)
+    my_tasks <- ldply(my_tasks, format_tasks)
+    if (dim(my_tasks)[1] > 0) {
+        if (!recurring) {
+            my_tasks <- my_tasks[-grep("^every", my_tasks$ds), 1:4]
+        }
+        if (recurring) {
+            my_tasks <- my_tasks[, 1:4]
+        }
+        temp <- ddply(my_tasks, .(id), summarize, due_date = format_date(due))
+        my_tasks <- join(my_tasks, temp, by = "id", type = "left")
+        my_tasks <- my_tasks[, -3]
+   return(my_tasks)
+    }
 }
-my_tasks <- fromJSON(tasks)
-my_tasks <- ldply(my_tasks, format_tasks)
-
-if(!recurring) {
-my_tasks <- my_tasks[-grep("^every", my_tasks$ds),1:4]
-}
-
-if(recurring) {
-	my_tasks <- my_tasks[,1:4]
-}
-temp <- ddply(my_tasks, .(id), summarize, due_date = format_date(due))
-my_tasks <- join(my_tasks,temp,by="id",type="left")
-my_tasks <- my_tasks[,-3]
-return (my_tasks)
-}
-
-
 #' get_tasks
 #'
 #' Returns a list of tasks from a specified project
@@ -42,25 +41,23 @@ return (my_tasks)
 #' @examples \dontrun{
 #' get_tasks(12345, token)
 #'}
-get_tasks <- function(project_id, token = getOption('TodoistToken')) {
-tasks_url <- "https://todoist.com"
+get_tasks <- function(project_id, token = getOption("TodoistToken")) {
+    tasks_url <- "https://todoist.com"
 }
-
-
 #'format_date
 #'
 #' @param date a todoist date
 #' @keywords internal
-#' @return Date
+#' @return \code{Date}
 format_date <- function(date_string) {
-date_string <- str_split_fixed(date_string, " ", 3)
-months <- c("Jan","Feb","Mar","Apr","May", "Jun","Jul","Aug","Sep","Oct","Nov","Dec")
-day <- str_sub(date_string[3],1,2)
-month <- date_string[2]
-m <- which(months==month)
-year <- date_string[3]
-date <- sprintf("%s-%s-%s",m,day,year)
-formatted_date <- as.Date(date, "%m-%d-%Y")
-return(formatted_date)
+    date_string <- str_split_fixed(date_string, " ", 3)
+    months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct",
+        "Nov", "Dec")
+    day <- str_sub(date_string[3], 1, 2)
+    month <- date_string[2]
+    m <- which(months == month)
+    year <- date_string[3]
+    date <- sprintf("%s-%s-%s", m, day, year)
+    formatted_date <- as.Date(date, "%m-%d-%Y")
+    return(formatted_date)
 }
-
